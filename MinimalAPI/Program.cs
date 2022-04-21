9b3d842a98b7821119be250d6fd06dfd732d5c65
georgekalokyris <kalokirisg@gmail.com>
2022-04-21 20:01:00 +0100

    Test

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader()
         .WithOrigins("http://localhost:3000", "htpps://appname.azurestaticapps.net");
     });
-});
+}); 
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
