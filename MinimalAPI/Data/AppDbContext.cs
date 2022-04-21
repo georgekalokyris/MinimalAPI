67c5829fe05f7eacadc91e41fc58734003c8838f
georgekalokyris <kalokirisg@gmail.com>
2022-04-21 19:46:39 +0100

    Prototype Created - Minimal API (No Controllers) & SQL Lite

---
+++ MinimalAPI/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI.Data
+{
+    internal sealed class AppDbContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder) => dbContextOptionsBuilder.UseSqlite("Data Source =./Data/MinimalApi.db");
+    
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Post[] postsToSeed = new Post[6];
+
+            for (int i = 0; i < postsToSeed.Length; i++)
+            {
+                postsToSeed[i] = new Post
+                {
+                    PostId = i+1,
+                    Title = $"Post {i+1}",
+                    Content = $"This is post {i+1} and it has some very interesting content.",
+                };
+            }
+
+            modelBuilder.Entity<Post>().HasData(postsToSeed);
+        }
+    }
+}
---
+++ MinimalAPI/Data/PostsRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalAPI.Data
+{
+    internal static class PostsRepository
+    {
+        internal async static Task<List<Post>> GetPostsAsync()
+        {
+            using (var db = new AppDbContext())
+            {
+                return await db.Posts.ToListAsync();
+            }
+        }
+
+        internal async static Task<Post> GetPostByIdAsync(int postId)
+        {
+            using (var db = new AppDbContext())
+            {
+                return await db.Posts.FirstOrDefaultAsync(x => x.PostId == postId);
+            }
+        }
+
+        internal async static Task<bool> CreatePostAsync(Post post)
+        {
+            using(var db = new AppDbContext())
+            {
+                try
+                {
+                    await db.Posts.AddAsync(post);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        
+        internal async static Task<bool> UpdatePostAsync(Post postToUpdate)
+        {
+            using(var db = new AppDbContext())
+            {
+                try
+                {
+                    db.Posts.Update(postToUpdate);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+        
+        internal async static Task<bool> DeletePostAsync(int postId)
+        {
+            using(var db = new AppDbContext())
+            {
+                try
+                {
+                    Post postToDelete = await GetPostByIdAsync(postId);
+
+                    db.Remove(postToDelete);
+
+                    return await db.SaveChangesAsync() >= 1;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+    }
+}
